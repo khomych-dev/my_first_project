8989967edc5b718ebc9541f68372692bcdc1a162
Anatolii Khomych <a.khomych.dev@gmail.com>
2026-03-02 11:16:14 +0200

    Реалізував завантаження даних з JSON

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from colorama import Fore, Style, init
+import json
+import os
 
 init()
 
@@ -21,28 +23,50 @@ class Car:
         return f"{color}{self.brand} {self.model} ({self.year}) -> {label}{Style.RESET_ALL}"
 
 
-init()
+def load_from_file():
+    file_name = 'garage.json'
+    loaded_garage = []
 
-# Наш порожній список (база даних у пам'яті)
-garage = []
+    if os.path.exists(file_name):
+        with open(file_name, 'r', encoding='utf-8') as f:
+            raw_data = json.load(f)
+
+        for item in raw_data:
+            loaded_garage.append(
+                Car(item['brand'], item['model'], item['year']))
+
+    return loaded_garage
+
+
+def save_to_file(garage_list):
+    if not garage_list:
+        return
+
+    data_to_save = []
+    for car in garage_list:
+        data_to_save.append(
+            {'brand': car.brand, 'model': car.model, 'year': car.year})
+
+    with open("garage.json", "w", encoding="utf-8") as f:
+        json.dump(data_to_save, f, ensure_ascii=False, indent=4)
+    print(Fore.BLUE + "Дані збережено у файл garage.json!" + Style.RESET_ALL)
+
+
+garage = load_from_file()
 
 print(Fore.CYAN + "=== СИСТЕМА УПРАВЛІННЯ ГАРАЖЕМ АКТИВОВАНА ===" + Style.RESET_ALL)
 print("Для виходу напиши 'стоп' у назві марки.\n")
 
 while True:
-    # 1. Запитуємо назву марки
     brand = input(
         Fore.YELLOW + "Введи марку авто (або 'стоп' для звіту): " + Style.RESET_ALL)
 
-    # 2. Умова виходу (наш "стоп-кран")
     if brand.lower() == 'стоп':
         break
 
-    # 3. Запитуємо інші дані
     model = input("Введи модель: ")
     year = input("Введи рік: ")
 
-    # Створюємо об'єкт і додаємо в список
     try:
         new_car = Car(brand, model, int(year))
         garage.append(new_car)
@@ -51,7 +75,6 @@ while True:
         print(
             Fore.RED + "× Помилка: Рік має бути числом! Спробуй ще раз.\n" + Style.RESET_ALL)
 
-# 4. Фінальний звіт після виходу з циклу
 print("\n" + Fore.CYAN + "="*30)
 print("ПІДСУМКОВИЙ ЗВІТ ГАРАЖА:")
 print("="*30 + Style.RESET_ALL)
@@ -61,5 +84,7 @@ if not garage:
 else:
     for car in garage:
         print(car.get_info())
+
+save_to_file(garage)
 
 print(Style.RESET_ALL)
